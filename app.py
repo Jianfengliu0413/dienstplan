4389d26d71d2b8e7ee6b37dbbf86b97961aa082b
Jianfengliu0413 <jianfeng.liu0413@gmail.com>
2026-08-05 20:53:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ st.set_page_config(
 
 # --- Fixed Rules file path ---
 RULES_FILE = "Rules_edit.xlsx"
-INACTIVITY_TIMEOUT_SECONDS = 30 # in seconds
+INACTIVITY_TIMEOUT_SECONDS = 300 # in seconds
 
 
 # --- Credentials ---
